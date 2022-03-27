cc2c32ce187f710bc484d455f77e2bf1537170cd
Abdullah <abdullah.5121011@gmail.com>
2022-03-27 13:45:25 -0400

    Number types

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Code typescript here...');
+// js
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 1;
+var num2 = 2;
+var num3 = '3';
+console.log(add(num1, num2));
+console.log(add(num1, num3));
+// ts
+function addts(n1, n2) {
+    return n1 + n2;
+}
+console.log(addts(num1, num2));
+// console.log(addts(num1, num3)); // wil produce an error
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('Code typescript here...')
+// js
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+const num1 = 1;
+const num2 = 2;
+const num3 = '3';
+
+console.log(add(num1, num2));
+console.log(add(num1, num3));
+
+// ts
+function addts(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+console.log(addts(num1, num2));
+// console.log(addts(num1, num3)); // wil produce an error
