4f5dbdcca66540a6d9ca1a0172744b479590327e
Abdullah <abdullah.5121011@gmail.com>
2022-03-27 13:55:26 -0400

    Boolean Type

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ var num2 = 2;
 var num3 = '3';
 console.log(add(num1, num2));
 console.log(add(num1, num3));
-// ts
+// ts number
 function addts(n1, n2) {
     return n1 + n2;
 }
 console.log(addts(num1, num2));
 // console.log(addts(num1, num3)); // wil produce an error
+// ts boolean
+function addtsb(n1, n2, donotReturn) {
+    if (donotReturn) {
+        console.log('The result is ' + (n1 + n2));
+        return;
+    }
+    return n1 + n2;
+}
+addtsb(num1, num2, false);
+addtsb(num1, num2, true);
--- app.ts
+++ app.ts
@@ -10,10 +10,22 @@ const num3 = '3';
 console.log(add(num1, num2));
 console.log(add(num1, num3));
 
-// ts
+// ts number
 function addts(n1: number, n2: number) {
     return n1 + n2;
 }
 
 console.log(addts(num1, num2));
 // console.log(addts(num1, num3)); // wil produce an error
+
+// ts boolean
+function addtsb(n1: number, n2: number, donotReturn: boolean) {
+    if (donotReturn) {
+        console.log(n1 + n2);
+        return;
+    }
+    return n1 + n2;
+}
+
+addtsb(num1, num2, false);
+addtsb(num1, num2, true);
